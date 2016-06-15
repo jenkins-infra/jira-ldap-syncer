41d64a7929864002d97b0dce5d826dc13226da95
Kohsuke Kawaguchi <kk@kohsuke.org>
2016-06-15 12:00:31 +0900

    Added a test driver code

---
+++ src/test/java/io/jenkins/backend/jiraldapsyncer/Foo.java
@@ -0,0 +1,19 @@
+package io.jenkins.backend.jiraldapsyncer;
+
+/**
+ * @author Kohsuke Kawaguchi
+ */
+public class Foo {
+    public static void main(String[] args) {
+        System.setProperty("jira.url","https://issues.jenkins-ci.org/");
+        System.setProperty("jira.username","kohsuke");
+        // System.setProperty("jira.password","...");    set this via JVM option
+        System.setProperty("url","ldap://localhost:9389");
+        System.setProperty("password","xxx");
+
+        JiraLdapSyncer jiraLdapSyncer = (JiraLdapSyncer) new ServiceLocator().lookupService(JiraLdapSyncer.ROLE);
+        RemoteUser jira = jiraLdapSyncer.getJiraRestClient().getUser("kohsuke");
+        System.out.println(jira);
+
+    }
+}
